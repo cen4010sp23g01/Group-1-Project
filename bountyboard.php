d3f863524d78559781f77bc55afdf0c7d020713a
Ethan F <122833449+efleming91@users.noreply.github.com>
2023-03-31 21:09:56 -0400

    Adding additional pages
    New pages:
    - Profile
    - Bounty Board
    - Calendar

--- bountyboard.php
+++ bountyboard.php
@@ -57,67 +57,9 @@
         </header>
         <!-- Home Section-->
         <section class="page-section portfolio" id="portfolio">
-            <div class="container">
-                <!-- Home Section Heading-->
-                <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Home</h2>
-                <!-- Icon Divider-->
-                <div class="divider-custom">
-                    <div class="divider-custom-line"></div>
-                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-                    <div class="divider-custom-line"></div>
-                </div>
-                <!-- Home Items-->
+            <div class="col-xl-10 order-xl-1 p-2 m-auto">
                 <div class="row justify-content-center">
-                    <div class="col-12 mb-5 text-center">
-                        Enter User ID into the textbox and press Submit to return User Name
-                    </div>
-                </div>
-                <div class="row justify-content-center">
-                    <div class="col-12 mb-5 text-center">
-                        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-                            <label for="uid">User ID (1-5):</label>
-                            <input type="number" id="uid" name="uid" min="1" max="5"><br><br>
-                            <input type="submit" value="Submit">
-                        </form>
-                    </div>
-                </div>
-                
-                <?php
-                    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                        $servername = "localhost";
-                        $username = "cen4010sp23g01";
-                        $password = "Gsp#12386";
-                        $dbname = "cen4010sp23g01";
-                        
-                        $uid = $_POST["uid"];
-                    
-                        // Create connection
-                        $conn = new mysqli($servername, $username, $password, $dbname);
-                        // Check connection
-                        if ($conn->connect_error) {
-                            die("Connection failed: " . $conn->connect_error);
-                        }
-
-                        $sql = "SELECT name FROM USER WHERE id = " . $uid;
-                        $result = $conn->query($sql);
-
-                        if ($result->num_rows > 0) {
-                            // output data of each row
-                            while($row = $result->fetch_assoc()) {
-                            $uname = $row["name"];
-                            }
-                        } else {
-                            echo "0 results";
-                        }
-                        $conn->close();
-                    }
-                ?>
-                
-                <div class="row justify-content-center">
-                    <div class="col-12 mb-5 text-center">
-                        <label for="uname">User Name:</label>
-                        <input type="text" id="uname" name="uname" value="<?php echo $uname;?>" disabled><br><br>
-                    </div>
+                    <img src="./assets/mockup_bounty.png" />
                 </div>
             </div>
         </section>
