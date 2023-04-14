f28e76b25363461b0b8210d1a7f3f2cc8733a899
Ethan F <122833449+efleming91@users.noreply.github.com>
2023-04-14 19:46:22 -0400

    Updates to Home, BB, Cal

--- index.php
+++ index.php
@@ -6,52 +6,52 @@
 <!-- Home Section-->
 <section class="page-section portfolio" id="portfolio">
     <div class="container">
-        <!-- Home Section Heading-->
-        <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Home</h2>
-        <!-- Icon Divider-->
-        <div class="divider-custom">
-            <div class="divider-custom-line"></div>
-            <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-            <div class="divider-custom-line"></div>
-        </div>
         <!-- Home Items-->
         <div class="row justify-content-center">
             <div class="col-12 mb-5 text-center">
-                Enter User ID into the textbox and press Submit to return User Name
-            </div>
-        </div>
-        <div class="row justify-content-center">
-            <div class="col-12 mb-5 text-center">
-                <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-                    <label for="uid">User ID (1-5):</label>
-                    <input type="number" id="uid" name="uid" min="1" max="5"><br><br>
-                    <input type="submit" value="Submit">
-                </form>
-            </div>
-        </div>
-        
-        <?php
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {    
-                $uid = $_POST["uid"];                
-
-                $sql = "SELECT userName FROM users WHERE userID = " . $uid;
-                $result = mysqli_query($conn, $sql);
-
-                if (mysqli_num_rows($result) > 0) {
-                    // output data of each row
-                    while($row = mysqli_fetch_assoc($result)) {
-                    $uname = $row["userName"];
-                    }
-                } else {
-                    echo "0 results";
-                }
-            }
-        ?>
-                
-        <div class="row justify-content-center">
-            <div class="col-12 mb-5 text-center">
-                <label for="uname">User Name:</label>
-                <input type="text" id="uname" name="uname" value="<?php echo $uname;?>" disabled><br><br>
+                <h1>Welcome to the Adventurer's Guild!</h1> 
+                <img class="img-profile" src="./assets/mascot.png" alt="Profile Image"> 
+                <div class="navbar navbar-expand-lg text-uppercase text-center">
+                    <?php
+                        if (isset($_SESSION["userid"]))
+                        {
+                            echo "
+                            <div class='navbar-collapse'>
+                                <ul class='navbar-nav ms-auto'>
+                                    <li class='nav-item mx-0 mx-lg-1'><a class='nav-link py-3 px-0 px-lg-3 rounded' href='profile.php'>Profile</a></li>
+                                </ul>
+                            </div>
+                            <div class='navbar-collapse'>
+                                <ul class='navbar-nav ms-auto'>
+                                    <li class='nav-item mx-0 mx-lg-1'><a class='nav-link py-3 px-0 px-lg-3 rounded' href='bountyboard.php'>Bounty Board</a></li>
+                                </ul>
+                            </div>
+                            <div class='navbar-collapse'>
+                                <ul class='navbar-nav ms-auto'>
+                                    <li class='nav-item mx-0 mx-lg-1'><a class='nav-link py-3 px-0 px-lg-3 rounded' href='calendar.php'>Calendar</a></li>
+                                </ul>
+                            </div>
+                            <div class='navbar-collapse'>
+                                <ul class='navbar-nav ms-auto'>
+                                    <li class='nav-item mx-0 mx-lg-1'><a class='nav-link py-3 px-0 px-lg-3 rounded' href='logout.php'>Logout</a></li>
+                                </ul>
+                            </div>";
+                        }
+                        else {
+                            echo "
+                            <div class='navbar-collapse'>
+                                <ul class='navbar-nav ms-auto'>
+                                    <li class='nav-item mx-0 mx-lg-1'><a class='nav-link py-3 px-0 px-lg-3 rounded' href='createaccount.php'>Create Account</a></li>
+                                </ul>
+                            </div>
+                            <div class='navbar-collapse'>
+                                <ul class='navbar-nav ms-auto'>
+                                    <li class='nav-item mx-0 mx-lg-1'><a class='nav-link py-3 px-0 px-lg-3 rounded' href='login.php'>Log In</a></li>
+                                </ul>
+                            </div>";
+                        }
+                    ?>
+                </div>
             </div>
         </div>
     </div>
