812120b7a24ab2982bd9c55b5859ecd72771d577
Ethan F <122833449+efleming91@users.noreply.github.com>
2023-04-14 18:25:23 -0400

    Updated Profile page & functionality

--- profile.php
+++ profile.php
@@ -4,14 +4,48 @@ include_once 'header.php';
 
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
 
-<body>
-    <!-- Page content -->
-        <div class="col-xl-10 order-xl-1 p-2 m-auto">
-            <div class="row justify-content-center">
-                    <img src="./assets/mockup_profile.png" />
+<!-- Profile Section-->
+<section class="page-section portfolio" id="portfolio">
+    <div class="container">
+        <!-- Profile Items-->
+        <div class="row justify-content-center">
+            <div class="col-12 mb-5 text-center">
+                <img class="img-profile" src="./assets/mascot.png" alt="Profile Image">
+                
+                <?php 
+                    $userID = $_SESSION["userid"];
+                    $userExp = $_SESSION["userexp"];
+                    $userLvl = $_SESSION["userlvl"];
+                
+                    require_once 'includes/dbh.inc.php';
+                
+                    $currExp = calcCurrent($userExp, $userLvl);
+                    $nextExp = calcNext($userLvl);
+                
+                    $totalBounties = calcCompleted($conn, $userID, 0);
+                    $easyBounties = calcCompleted($conn, $userID, 1);
+                    $mediumBounties = calcCompleted($conn, $userID, 2);
+                    $hardBounties = calcCompleted($conn, $userID, 3);
+                ?>
+                
+                <h2><?php echo $_SESSION["username"];?></h2>
+                <div class="xp-bar">
+                    <div class="xp-progress"></div>
+                </div>
+                <h3>Level <?php echo $userLvl;?></h3>
+                <h5><?php echo $currExp;?>/<?php echo $nextExp;?> XP</h5>
+                <div class="completed-bounties">
+                    Completed Bounties: <?php echo $totalBounties;?><br> 
+                    Easy: <?php echo $easyBounties;?><br>
+                    Medium: <?php echo $mediumBounties;?><br>
+                    Hard: <?php echo $hardBounties;?><br>
+                </div>
+
+                <!--<a href="completed-bounties.html">Completed Bounties</a>  Add hyperlink to bountyboard -->
             </div>
         </div>
-</body>
+    </div>
+</section>
 
 <?php
   include_once 'footer.php';
