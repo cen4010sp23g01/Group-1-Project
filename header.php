22a739ef36b855933300962b021067cbc6b9ed8d
Ethan F <122833449+efleming91@users.noreply.github.com>
2023-04-30 20:13:57 -0400

    Fixing about file type

--- header.php
+++ header.php
@@ -74,7 +74,7 @@
                 ?>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="https://lamp.cse.fau.edu/~cen4010-sp23-g01/about/about.php">About</a></li>
+                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="https://lamp.cse.fau.edu/~cen4010-sp23-g01/about/about.html">About</a></li>
                     </ul>
                 </div>
             </div>
